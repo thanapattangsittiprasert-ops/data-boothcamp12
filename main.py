a756f4cd2f449f718c9ec1f77ec138e6b310dc2f
thanapattangsittiprasert-ops <thanapat.tangsittiprasert@gmail.com>
2025-12-22 11:22:54 +0700

    Now I know it.

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 # print hello world
-print("hello world")
-
-# print good bye
-print("good bye")
-
-# print new line
-print("I'm learning github.")
-
-def add_two(num):
-  return num+2
-
-print(add_two(3), add_two(5))
+print("Now I know what git and git hub is. Easy!!")
